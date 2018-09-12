4046cad966f628cef0a862bbd6e8909a4b587566
Aaron Rosenmund <aaron.rosenmund@gmail.com>
2018-09-12 15:59:13 -0500

    Update and rename deploy_NBAD to deploy_NBAD.sh

---
+++ NBAD/deploy_NBAD.sh
@@ -0,0 +1,20 @@
+#####Coming Soon!####
+
+
+
+#Download Elasticsearch 6.4
+sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.0.rpm 
+
+#Download Moloch 1.1.2
+
+#Download Analysis Pipeline 5.8
+
+#
+
+
+
+
+#Install Elasticsearch
+sudo rpm --install elasticsearch-6.4.0.rpm
+#Configure Elasticsearch
+#sed /etc/elasticsearch/elasticsearch.yml
